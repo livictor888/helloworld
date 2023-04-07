465811b6f28ecb6fbdfea6c57b253695a6dec333
victorli888 <vli30@my.bcit.ca>
2023-04-06 20:19:46 -0700

    update urls.py for results variables

--- pages/urls.py
+++ pages/urls.py
@@ -2,13 +2,15 @@
 from django.urls import path, include
 from .views import homePageView, aboutPageView, victorPageView, results, homePost, todos, register, message, secretArea
 
+
 urlpatterns = [
     path('', homePageView, name='home'),
     path('about/', aboutPageView, name='about'),
     path('victor/', victorPageView, name='victor'),
     path('homePost/', homePost, name='homePost'),
     path('<int:choice>/results/', results, name='results'),
-    path('results/<int:choice>/<str:gmat>/', results, name='results'),
+    # path('results/<int:choice>/<str:gmat>/', results, name='results'),
+    path('results/<int:length>/<int:margin_low>/<int:margin_up>/<int:diagonal>/', results, name='results'),
     path('todos', todos, name='todos'),
     path("register/", register, name="register"),
     path('message/<str:msg>/<str:title>/', message, name="message"),
